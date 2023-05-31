506a178a699f8f2aecad4fa6b3d22146d4252f79
Tron <67748834+moneshkovi@users.noreply.github.com>
2023-05-31 18:32:06 +0530

    Update train-erc-text.py

--- train-erc-text.py
+++ train-erc-text.py
@@ -39,7 +39,7 @@ def main(
     logging.info(
         f"automatic hyperparameter tuning with speaker_mode: {speaker_mode}, "
         f"num_past_utterances: {num_past_utterances}, "
-        f"num_future_utterances: {num_future_utterances}"
+        f"num_future_utterances: {num_future_utterances}"   
     )
     CURRENT_TIME = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 
@@ -67,7 +67,7 @@ def main(
             [
                 "python3",
                 "train-erc-text-full.py",
-                "--OUTPUT-DIR",
+                "--OUTPUT-DIR",    
                 OUTPUT_DIR,
                 "--SEED",
                 str(SEED),
